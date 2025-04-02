4aab9291d8a812edf32a892f17a5e8d2abc1cc96
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-02 15:16:46 +0530

    feat: impl pokemon-data-provider

--- lib/src/core/providers/pokemon_data_provider.dart
+++ lib/src/core/providers/pokemon_data_provider.dart
@@ -1,18 +1,35 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pokemonverse/src/features/home/data/models/pokemon.dart';
 import 'package:pokemonverse/src/core/services/http_service.dart';
 
-final pokemonDataProvider = FutureProvider.family<Pokemon?, String>((
-  ref,
-  url,
-) async {
-  HttpService httpService = GetIt.instance.get<HttpService>();
-  Response? response = await httpService.get(url);
+/// A Riverpod provider that fetches Pokemon data from a given URL
+final pokemonDataProvider = FutureProvider.family<Pokemon?, String>(
+  (ref, String url) async {
+    try {
+      final httpService = GetIt.I<HttpService>();
+      final Response? response = await httpService.get(url);
 
-  if (response != null && response.data != null) {
-    return Pokemon.fromJson(response.data!);
+      if (response?.data == null) {
+        _logDebug('No data received for URL: $url');
+        return null;
+      }
+
+      return Pokemon.fromJson(response!.data!);
+    } on DioException catch (dioError) {
+      _logDebug('Dio error fetching Pokemon: ${dioError.message}');
+      return null;
+    } catch (e, stackTrace) {
+      _logDebug('Unexpected error: $e\n$stackTrace');
+      return null;
+    }
+  },
+);
+
+void _logDebug(String message) {
+  if (kDebugMode) {
+    debugPrint(message);
   }
-  return null;
-});
+}
