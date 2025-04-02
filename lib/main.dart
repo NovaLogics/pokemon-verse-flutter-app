6b591a70a8aaec5aa2bdb652ce8eaf94ed322452
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-02 14:12:46 +0530

    refactor: main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:pokemonverse/src/app.dart';
 import 'package:pokemonverse/src/data/services/database_service.dart';
 import 'package:pokemonverse/src/data/services/http_service.dart';
-import 'package:pokemonverse/src/presentation/home_screen.dart';
 
 void main() async {
   await _setupServices();
-  runApp(const MyApp());
+
+  _launchApp();
 }
 
 Future<void> _setupServices() async {
@@ -16,23 +15,6 @@ Future<void> _setupServices() async {
   GetIt.instance.registerSingleton<DatabaseService>(DatabaseService());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        title: 'Pokemon-Verse',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-          useMaterial3: true,
-          textTheme: GoogleFonts.quattrocentoSansTextTheme(),
-        ),
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
-    );
-  }
+void _launchApp() {
+  runApp(const PokemonVerseApp());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pokemonverse/main.dart';
+import 'package:pokemonverse/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PokemonVerseApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
